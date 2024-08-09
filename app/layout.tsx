bf8549d5901bff81a73621cfe7dc663ecbdc875d
Raamponsah <raamponsah@gimpa.edu.gh>
2024-08-09 17:12:18 +0000

    updating home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-200 text-lg`}>{children}</body>
+      <body className={`${inter.className} bg-slate-200 text-lg min-h-screen`}>
+        <header className="flex justify-between items-center px-20 py-5">
+          <h1 className="font-bold">Workily</h1>
+          <span className="bg-orange-400 h-4 w-4 text-xs flex justify-center items-center rounded-full">A</span>
+        </header>
+        
+        {children}
+        
+        </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+
 export default function Page(){
-  return <h1>Workily</h1>
+  return <div className="flex justify-center items-center h-screen">
+<Link href="" className="bg-blue-300 border-2 border-gray-900 shadow-lg rounded-lg px-4 py-2">Create a space</Link>
+  </div>
 }
